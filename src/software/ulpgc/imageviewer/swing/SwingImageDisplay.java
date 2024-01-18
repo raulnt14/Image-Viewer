16e17ecc9d7b52ecdd6f817c509c040b9c6ab55d
Raúl Núñez <raul.nunez101@alu.ulpgc.es>
2024-01-18 22:01:06 +0000

    Image management interfaces implemented

---
+++ src/software/ulpgc/imageviewer/swing/SwingImageDisplay.java
@@ -0,0 +1,27 @@
+package software.ulpgc.imageviewer.swing;
+
+import software.ulpgc.imageviewer.ImageDisplay;
+
+import javax.swing.*;
+
+public class SwingImageDisplay extends JPanel implements ImageDisplay {
+    @Override
+    public void paint(String id, int offset) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void on(Shift shift) {
+
+    }
+
+    @Override
+    public void on(Released released) {
+
+    }
+}
